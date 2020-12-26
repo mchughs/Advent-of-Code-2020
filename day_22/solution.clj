6922a7746f908794505843f22bd50f67c2682f4a
Samuel McHugh <smchugh230395@gmail.com>
2020-12-26 09:29:16 +0100

    solution to day 22 part 2 by rjray

--- day_22/solution.clj
+++ day_22/solution.clj
@@ -43,7 +43,64 @@
           (* i n)))
       0)))
 
-^{:meta "part1"}
+; ^{:meta "part1"}
 (calculate-score combat-winning-deck)
 
 ;---
+
+;; part 2 solution lifted rjray
+
+(defn- score-game [game]
+  (let [deck (:winner game)]
+    (reduce + (map * (reverse deck) (iterate inc 1)))))
+
+(defn- play-game-rec [game]
+  (let [p1 (:p1 game)
+        p2 (:p2 game)]
+    (loop [p1 p1, p2 p2, seen #{}]
+      (if (seen (list p1 p2))
+        {:winner :p1, :p1 p1}
+        (let [[n1 & p1'] p1, [n2 & p2'] p2]
+          (cond
+            (nil? n1) {:winner :p2, :p2 p2}
+            (nil? n2) {:winner :p1, :p1 p1}
+            (and (<= n1 (count p1'))
+                 (<= n2 (count p2')))
+            (let [game' (play-game-rec {:p1 (take n1 p1')
+                                        :p2 (take n2 p2')})]
+              (case (:winner game')
+                :p1 (recur (concat p1'
+                                   (list n1 n2))
+                           p2'
+                           (conj seen (list p1 p2)))
+                :p2 (recur p1'
+                           (concat p2'
+                                   (list n2 n1))
+                           (conj seen (list p1 p2)))))
+            (< n1 n2) (recur p1'
+                             (concat p2'
+                                     (list n2 n1))
+                             (conj seen (list p1 p2)))
+            :else     (recur (concat p1'
+                                     (list n1 n2))
+                             p2'
+                             (conj seen (list p1 p2)))))))))
+
+(defn- score-game-rec [game]
+  (score-game {:winner (game (:winner game))}))
+
+(defn- parse-data [[p1 p2]]
+  (let [[_ & lines1] (s/split-lines p1)
+        [_ & lines2] (s/split-lines p2)]
+    {:p1 (map read-string lines1)
+     :p2 (map read-string lines2)}))
+
+(defn part-2
+  "Day 22 Part 2"
+  [input]
+  (-> input
+      parse-data
+      play-game-rec
+      score-game-rec))
+
+; (part-2 input)
