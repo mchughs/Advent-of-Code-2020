76bdd4c77eca8934b15be491bae2f35d91abe01d
Samuel McHugh <smchugh230395@gmail.com>
2020-12-26 09:16:27 +0100

    day 20 part 2 solution by fred overflow

--- day_20/solution.clj
+++ day_20/solution.clj
@@ -7,9 +7,7 @@
             [utils :as u]))
 
 (def input
-  (-> "day_20/input.txt"
-    slurp
-    (s/split #"\n\n")))
+  (slurp "day_20/input.txt"))
 
 (defn parse-tile [tile-str]
   (let [id (Long. (re-find #"\d+" tile-str))
@@ -26,7 +24,9 @@
      ;; arranged clockwise: top right bottom left
      :borders borders}))
 
-(def tiles (map parse-tile input))
+(def tiles
+  (map parse-tile
+    (s/split input #"\n\n")))
 
 (defn rotate-counter-clockwise [tile]
   (update tile :borders
@@ -66,87 +66,118 @@
 
 ;---
 
-(defn rotations [borders]
-  (->> borders
-    (map (comp s/join reverse))
-    (concat borders)
-    set))
+;; Part 2 lifted from Fred Overflow
 
-(rotations ["###.#..###" "#######.##" ".##..##..#" "##..###.#."])
+(defn border [^String tile ^long start ^long end ^long step]
+  (s/join
+    (map #(.charAt tile %)
+      (range start end step))))
 
-(defn find-matches [{:keys [id borders] :as tile}]
-  (let [tiles' (->> tiles
-                 (remove #(= % tile))
-                 (map #(update % :borders rotations)))
-        connected (->> borders
-                    (map (fn [border]
-                           (let [{connected-id :id} (u/find-first
-                                                      #(contains? (:borders %) border)
-                                                      tiles')]
-                              connected-id)))
-                    (remove nil?))]
-    {id connected}))
+(defn parse-tiles [^String input]
+  (for [[_ id ^String raw] (re-seq #"Tile (\d+):\n([.#\n]{109})" input)]
+    {:id     (Integer/parseInt id)
+     :top    (border raw 0  10  1)
+     :bottom (border raw 99 109 1)
+     :left   (border raw 0  110 11)
+     :right  (border raw 9  119 11)
+     :payload (s/join
+                (for [y (range 11 99 11)]
+                  (.substring raw (+ y 1) (+ y 9))))}))
 
-(def connection-map
+(defn rotate
+  "Clockwise"
+  [{:keys [id top bottom left right ^String payload]}]
+  {:id id
+   :top right
+   :right (s/reverse bottom)
+   :bottom left
+   :left (s/reverse top)
+   :payload (s/join
+              (for [x (range 7 -1 -1)
+                    y (range 0 64 8)]
+                (.charAt payload (+ y x))))})
+
+(defn flip
+  [{:keys [id top bottom left right ^String payload]}]
+  {:id id
+   :top bottom
+   :bottom top
+   :right (s/reverse right)
+   :left (s/reverse left)
+   :payload (s/join
+              (for [y (range 56 -8 -8)]
+                (.substring payload y (+ y 8))))})
+
+(defn symmetries [a]
+  (let [b (rotate a)
+        c (rotate b)
+        d (rotate c)
+        e (flip a)
+        f (flip b)
+        g (flip c)
+        h (flip d)]
+    [a b c d e f g h]))
+
+(defn find-neighbor
+  "Returns the single matching tile which lines up with
+   a particular tile's edge (origin) in the given direction."
+  [origin direction opposite tiles]
+  (let [[neighbor]
+        (filter (fn [tile]
+                  (and
+                    (= (direction origin) (opposite tile))
+                    (not= (:id origin) (:id tile))))
+          tiles)]
+    neighbor))
+
+(defn find-edge
+  "Walk to edge in a certain direction."
+  [origin direction opposite tiles]
+  (let [neighbor (find-neighbor origin direction opposite tiles)]
+    (if neighbor
+      (recur neighbor direction opposite tiles)
+      origin)))
+
+;---
+
+(defn assemble-grid [tiles]
+  (let [[somewhere] tiles
+        some-top (find-edge somewhere :top  :bottom tiles)
+        top-left (find-edge some-top  :left :right tiles)
+        left-edge (take-while some?
+                    (iterate #(find-neighbor % :bottom :top tiles) top-left))]
+    (for [left left-edge]
+      (for [tile (take-while some?
+                   (iterate #(find-neighbor % :right :left tiles) left))]
+        tile))))
+
+(defn assemble-image [grid]
+  (s/join
+    (for [line grid
+          y (range 0 64 8)
+          tile line]
+      (-> tile :payload (.substring y (+ y 8))))))
+
+(def monster
+  (-> "
+   (. . . . . . . . . . . . . . . . . .)#(. .{76}
+   )#(. . . .)# #(. . . .)# #(. . . .)# # #(.{76})
+   (.)#(. .)#(. .)#(. .)#(. .)#(. .)#(. . . .{76})
+   "
+   (s/replace #"\s" "")
+   re-pattern))
+
+(def submerge "$1.$2.$3..$4..$5...$6$7.$8.$9.$10.$11.$12.$13")
+
+(for [tiles (->> input
+              parse-tiles
+              (mapcat symmetries)
+              (iterate next)
+              (take 8))]
   (->> tiles
-    (map find-matches)
-    (reduce merge)))
-
-; (defn construct-outer-ring [m]
-;   (let [pieces (->> m
-;                  (remove #(< 3 (count (last %))))
-;                  (reduce (fn [acc [k v]]
-;                            (update acc (count v) conj {k v}))
-;                    {2 '()
-;                     3 '()}))
-;         corners (reduce merge (get pieces 2))
-;         edges   (reduce merge (get pieces 3))]
-;     [corners edges]))
-
-; (construct-outer-ring connection-map)
-
-(def side-length
-  (-> tiles count Math/sqrt int))
-
-; (def template
-;   (->> nil
-;     (repeat side-length)
-;     (repeat side-length)))
-
-(defn neighbors [i l]
-  (let [indeces (filter #(< -1 % (count tiles))
-                  [(inc i)
-                   (dec i)
-                   (+ i side-length)
-                   (- i side-length)])]
-    (->> indeces
-      (map #(nth l %))
-      (remove nil?))))
-
-(loop [options connection-map
-       grid (repeat (count tiles) nil)]
-  (if-not (some nil? (flatten grid))
-    grid
-    (let [i (.indexOf nil grid)
-          restrictions (neighbors i grid)]
-      (if (empty? restrictions)
-        (recur (->> options (drop 0) (reduce merge {}))
-          (assoc grid i (first options)))
-
-        ())
-      (recur
-        (dissoc)))))
-
-    ; (let [[i j]]
-    ;   body)
-    ; (recur (dissoc options))))
-
-
-; (def first-row
-;   (let [first-corner (u/find-first (fn [[k v]] (= 2 (count v))) connection-map)]
-;     (loop [m (remove #(= % first-corner) connection-map)
-;            row [(first first-corner)]]
-;       (if (empty? m)
-;         row
-;         (let [id (first row)]
-;           (u/find-first (fn [[k v]] (and (= 3 (count v))) connection-map)))))))
+    assemble-grid
+    assemble-image
+    (#(s/replace % monster submerge))
+    (#(s/replace % monster submerge))
+    (filter #(= \# %))
+    count))
